a02ed7ef09b071065ed5a65b60a63943d1142b70
TheDudeFromCI <thedudefromci@gmail.com>
2023-10-29 23:37:49 -0700

    Moved client and server into features
    Signed-off-by: TheDudeFromCI <thedudefromci@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,26 @@
 use bevy::app::PluginGroupBuilder;
 use bevy::prelude::*;
-use gamestates::{ClientGameStatePlugin, ServerGameStatePlugin};
 
 pub mod gamestates;
 
+#[cfg(feature = "client")]
 #[derive(Debug, Default)]
 pub struct ClientPlugins;
 impl PluginGroup for ClientPlugins {
     fn build(self) -> PluginGroupBuilder {
+        use gamestates::client::ClientGameStatePlugin;
+
         PluginGroupBuilder::start::<Self>().add(ClientGameStatePlugin)
     }
 }
 
+#[cfg(feature = "server")]
 #[derive(Debug, Default)]
 pub struct ServerPlugins;
 impl PluginGroup for ServerPlugins {
     fn build(self) -> PluginGroupBuilder {
+        use gamestates::server::ServerGameStatePlugin;
+
         PluginGroupBuilder::start::<Self>().add(ServerGameStatePlugin)
     }
 }
